64ff16a38f0cebf1d0ec5bd4f92313b07068a44f
Shaharyar Shakir <shakirshaharyar125@gmail.com>
2025-03-28 13:15:42 +0500

    Adding files of nvim

---
+++ nvim/lua/config/settings.lua
@@ -0,0 +1,5 @@
+-- settings.lua
+vim.opt.termguicolors = true -- Enable true colors
+vim.opt.background = "dark"  -- Set background (can be "light" too)
+vim.cmd.colorscheme("melange") -- Apply Melange theme
+
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "savq/melange-nvim",
+    lazy = false, -- Ensure it's loaded immediately
+    priority = 1000, -- Load before other plugins
+  },
+}
+
