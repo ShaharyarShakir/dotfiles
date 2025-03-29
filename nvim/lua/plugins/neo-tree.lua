a78df77dc7ee82584ab90cef3a3c74ec5e87de62
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-03-29 22:06:46 +0500

    adding plugins in neovim

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,8 +6,8 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+            config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
--- nvim/lua/plugins/telescop.lua
+++ nvim/lua/plugins/telescop.lua
@@ -6,20 +6,23 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opt = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
+    opts = {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({}),
         },
-      })
+      },
+    },
+    config = function()
+      local telescope = require("telescope")
       local builtin = require("telescope.builtin")
+
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
-      require("telescope").load_extension("ui-select")
+      telescope.load_extension("ui-select")
     end,
   },
 }
+
