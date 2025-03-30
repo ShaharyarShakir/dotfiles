802d3394bfbd51b445b6ced77c6397553e9541eb
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-03-30 12:44:41 +0500

    adding improve yazi

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,7 @@ return {
 	},
             config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+		vim.keymap.set("n", "<leader>r", ":Neotree filesystem focus left<CR>",{})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
