6991afc2ba3217e684eb11a322cc642316946d5b
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-04-02 21:21:48 +0500

    Updated: nvim/lazy-lock.json nvim/lua/plugins/lsps/mason-lsp.lua nvim/lua/plugins/vim-tmux-navigator.lua

--- nvim/lua/plugins/lsps/mason-lsp.lua
+++ nvim/lua/plugins/lsps/mason-lsp.lua
@@ -1,24 +1,24 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  lazy = false,
-  dependencies = { "neovim/nvim-lspconfig" },
-  config = function()
-    local mason_lspconfig = require("mason-lspconfig")
+	"williamboman/mason-lspconfig.nvim",
+	lazy = false,
+	dependencies = { "neovim/nvim-lspconfig" },
+	config = function()
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
-      },
-    })
-  end,
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"markdown_oxide",
+			},
+		})
+	end,
 }
-
