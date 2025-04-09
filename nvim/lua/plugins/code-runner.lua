f3601b1bbdf714c1e296f5105ce07f9a2bdd753b
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-04-09 22:25:49 +0500

    modify the runner for which key

--- nvim/lua/plugins/code-runner.lua
+++ nvim/lua/plugins/code-runner.lua
@@ -1,14 +1,33 @@
 return {
-	"CRAG666/code_runner.nvim",
-	lazy = true,
-	config = function ()
-		require("lazy").setup({})
-		vim.keymap.set('n', '<leader>rr', ':RunCode<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
-	end
+  "CRAG666/code_runner.nvim",
+  lazy = true,
+  config = function()
+    require("code_runner").setup({
+      mode = "float", -- Show output in floating terminal
+      startinsert = true,
+      filetype = {
+        python = "python3 -u",
+        cpp = "g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+        c = "gcc $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+        java = "javac $fileName && java $fileNameWithoutExt",
+        javascript = "node",
+        typescript = "ts-node",
+        rust = "cargo run",
+        go = "go run",
+        sh = "bash",
+      },
+      project = {
+        -- Optional: Add custom projects here
+      },
+    })
+
+    vim.keymap.set("n", "<leader>rr", ":RunCode<CR>", { noremap = true, silent = false, desc = "Run Last Code" })
+    vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false, desc = "Run Current File" })
+    vim.keymap.set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false, desc = "Run File in Tab" })
+    vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false, desc = "Run Project" })
+    vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false, desc = "Close Runner" })
+    vim.keymap.set("n", "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false, desc = "Show Filetype Command" })
+    vim.keymap.set("n", "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false, desc = "Show Projects List" })
+  end,
 }
+
--- nvim/lua/plugins/vim-vs.lua
+++ nvim/lua/plugins/vim-vs.lua
@@ -1,6 +1,14 @@
+
 return {
   "mg979/vim-visual-multi",
   branch = "master",
-  lazy = false, -- Load immediately, or set to true if you want to lazy load
+  lazy = false, -- Load immediately
+  config = function()
+    vim.keymap.set('n', '<C-n>', '<Plug>(VM-Start)', { noremap = true, silent = true })
+    vim.keymap.set('x', '<C-n>', '<Plug>(VM-Start)', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-d>', '<Plug>(VM-SelectNext)', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-u>', '<Plug>(VM-SelectPrev)', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-Down>', '<Plug>(VM-AddCursorDown)', { noremap = true, silent = true })
+    vim.keymap.set('n', '<C-Up>', '<Plug>(VM-AddCursorUp)', { noremap = true, silent = true })
+  end
 }
-
