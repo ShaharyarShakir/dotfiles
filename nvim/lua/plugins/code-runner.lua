abe6fb97f262b5345d78004608c503495115c344
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-04-09 22:04:38 +0500

    Updated: nvim/lazy-lock.json nvim/lua/plugins/code-runner.lua tmux/.tmux.conf

---
+++ nvim/lua/plugins/code-runner.lua
@@ -0,0 +1,14 @@
+return {
+	"CRAG666/code_runner.nvim",
+	lazy = true,
+	config = function ()
+		require("lazy").setup({})
+		vim.keymap.set('n', '<leader>rr', ':RunCode<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
+	end
+}
