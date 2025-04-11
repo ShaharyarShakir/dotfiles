0b538a56f5f3e629c41b83ae034f6db0263e8904
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-04-11 17:59:40 +0500

    Updated: nvim/lazy-lock.json nvim/lua/plugins/simulator.lua nvim/lua/plugins/vim-vs.lua

--- nvim/lua/plugins/vim-vs.lua
+++ nvim/lua/plugins/vim-vs.lua
@@ -1,14 +1,21 @@
-
 return {
-  "mg979/vim-visual-multi",
-  branch = "master",
-  lazy = false, -- Load immediately
-  config = function()
-    vim.keymap.set('n', '<C-n>', '<Plug>(VM-Start)', { noremap = true, silent = true })
-    vim.keymap.set('x', '<C-n>', '<Plug>(VM-Start)', { noremap = true, silent = true })
-    vim.keymap.set('n', '<C-d>', '<Plug>(VM-SelectNext)', { noremap = true, silent = true })
-    vim.keymap.set('n', '<C-u>', '<Plug>(VM-SelectPrev)', { noremap = true, silent = true })
-    vim.keymap.set('n', '<C-Down>', '<Plug>(VM-AddCursorDown)', { noremap = true, silent = true })
-    vim.keymap.set('n', '<C-Up>', '<Plug>(VM-AddCursorUp)', { noremap = true, silent = true })
-  end
+	"mg979/vim-visual-multi",
+	branch = "master",
+	lazy = false, -- Load immediately
+	config = function()
+		local keymap = vim.keymap.set
+		local opts = { noremap = true, silent = true }
+
+		-- Use <leader>n to start multi-selection on word under cursor
+		keymap("n", "<leader>n", "<Plug>(VM-Find-Under)", opts)
+		keymap("x", "<leader>n", "<Plug>(VM-Find-Subword-Under)", opts)
+
+		-- Select next and previous occurrences
+		keymap("n", "<leader>d", "<Plug>(VM-SelectNext)", opts)
+		keymap("n", "<leader>u", "<Plug>(VM-SelectPrev)", opts)
+
+		-- Add cursors up/down
+		keymap("n", "<leader>j", "<Plug>(VM-AddCursorDown)", opts)
+		keymap("n", "<leader>k", "<Plug>(VM-AddCursorUp)", opts)
+	end,
 }
