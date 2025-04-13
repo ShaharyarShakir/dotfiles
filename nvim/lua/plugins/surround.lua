95e329676c83a7be731f15d1c0302c3f76d8340d
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-04-13 19:26:37 +0500

    Updated: nvim/lua/plugins/comments.lua nvim/lua/plugins/surround.lua

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,7 +1,15 @@
-return{
+return {
 	"kylechui/nvim-surround",
-  event = { "BufReadPre", "BufNewFile" },
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  config = true,
-		
+	event = { "BufReadPre", "BufNewFile" },
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	config = true,
+--	NORMAL mode
+--Keybinding	Action	Example
+--ys{motion}{char}	Yank + Surround (adds surround)	ysiw" → "word"
+--s{old}{new}	Change Surrounding from one to another	cs"' → 'word'
+--ds{char}	Delete Surrounding	ds" → word
+-- Visual Mode
+-- to surround multiple lines 
+-- first go to visual mode select the number of line then capital St then add the element
+
 }
