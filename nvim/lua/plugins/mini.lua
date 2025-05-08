8e9ff57e485bd0d0f03d5a554e97ada08df3fb03
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-05-08 23:59:02 +0500

    added the mini explorer

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -116,14 +116,17 @@ return {
     },
     -- Split & join
     {
-        "echasnovski/mini.splitjoin",
-        config = function()
-            local miniSplitJoin = require("mini.splitjoin")
-            miniSplitJoin.setup({
-                mappings = { toggle = "" }, -- Disable default mapping
-            })
-            vim.keymap.set({ "n", "x" }, "sj", function() miniSplitJoin.join() end, { desc = "Join arguments" })
-            vim.keymap.set({ "n", "x" }, "sk", function() miniSplitJoin.split() end, { desc = "Split arguments" })
-        end,
+
+        	"echasnovski/mini.splitjoin",
+
+        	config = function()
+            	local miniSplitJoin = require("mini.splitjoin")
+            	miniSplitJoin.setup({
+                	mappings = { toggle = "" }, -- Disable default mapping
+            	})
+            	vim.keymap.set({ "n", "x" }, "sj", function() miniSplitJoin.join() end, { desc = "Join arguments" })
+            	vim.keymap.set({ "n", "x" }, "sk", function() miniSplitJoin.split() end, { desc = "Split arguments" })
+        	end,
+
     },
 }
