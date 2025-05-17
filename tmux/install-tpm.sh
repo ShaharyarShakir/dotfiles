1ade92558613e8765452b186bf3580439847b3fa
ShaharyarShakir <shakirshaharyar125@gmail.com>
2025-05-17 19:17:58 +0500

    updated

--- tmux/install-tpm.sh
+++ tmux/install-tpm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 set -e
-
 if ! pacman -Q tmux &>/dev/null; then
   echo "tmux is not installed."
   exit 1
@@ -14,7 +12,7 @@ if [ -d "$TPM_DIR" ]; then
   echo "TPM is already installed in $TPM_DIR"
 else
   echo "Installing Tmux Plugin Manager (TPM)..."
-  git clone https://github.com/tmux-plugins/tpm $TPM_DIR
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 fi
 
 echo "TPM installed successfully!"
@@ -22,7 +20,7 @@ echo "Now opening tmux session and installing plugins..."
 
 tmux new-session -d -s tpm_install_session
 
-# I use C-s as my prefix for tmux. if you don't have that. change this line
+# I use C-b as my prefix for tmux. if you don't have that. change this line
 tmux send-keys -t tpm_install_session C-b "I" C-m
 
 tmux attach -t tpm_install_session
