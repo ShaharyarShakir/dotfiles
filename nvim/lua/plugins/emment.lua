db2b5569f4105abe3899b02118f3ce3b1dbbb7ea
shaharyarshakir <shakirshaharyar125@gmail.com>
2025-10-18 06:34:20 +0500

    updated nvim lua: emment

--- nvim/lua/plugins/emment.lua
+++ nvim/lua/plugins/emment.lua
@@ -1,8 +1,34 @@
 return {
-    -- here only for wrapping html tags
-    -- emmet_ls is already installed in mason
-        "olrtg/nvim-emmet",
-        config = function()
-            vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
-        end,
+  "olrtg/nvim-emmet",
+  config = function()
+    local emmet = require("nvim-emmet")
+
+    local function safe_wrap()
+      local ft = vim.bo.filetype
+
+      if ft == "svelte" then
+        -- Temporarily override Treesitter parser for this buffer
+        vim.treesitter.stop()
+        vim.treesitter.language.register('html', 'svelte')
+      end
+
+      -- Schedule so the Treesitter change takes effect before Emmet runs
+      vim.schedule(function()
+        emmet.wrap_with_abbreviation()
+
+        -- Restore original parser afterwards
+        if ft == "svelte" then
+          vim.schedule(function()
+            -- Restart Treesitter with correct parser
+            vim.treesitter.stop()
+            vim.cmd("set filetype=svelte")
+          end)
+        end
+      end)
+    end
+
+    vim.keymap.set({ "n", "v" }, "<leader>xe", safe_wrap,
+      { desc = "Emmet wrap with abbreviation (safe)" })
+  end,
 }
+
