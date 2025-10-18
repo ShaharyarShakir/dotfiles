49e33decfa30f634ad143b5c2652a42571a8076d
shaharyarshakir <shakirshaharyar125@gmail.com>
2025-10-18 06:34:38 +0500

    updated nvim lua plugin: flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,9 +1,17 @@
-return 
-	{
+return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+      },
+      char = {
+        jump_labels = true,
+      },
+    },
+  },
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -12,5 +20,4 @@ return
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
-
 }
