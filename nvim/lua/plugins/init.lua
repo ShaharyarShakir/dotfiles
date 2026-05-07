578e3a4a38400637be102e892c47f787f0d27f12
Shaharyar Shakir <shakirshaharyar125@gmail.com>
2026-05-07 20:06:22 +0500

    deleted stuff

--- nvim/lua/plugins/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
-	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-	"nvim-tree/nvim-web-devicons",  -- wev cons
-}
